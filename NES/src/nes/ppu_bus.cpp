244e6b0cdbfa0d18d62e78b515d6a14ed346c672
Rhett <rhett.newton@gmail.com>
2019-03-10 09:59:21 -0500

    update namespace reference

--- NES/src/nes/ppu_bus.cpp
+++ NES/src/nes/ppu_bus.cpp
@@ -1,40 +1,43 @@
 #include <nes\ppu_bus.h>
 #include <nes\cartridge\cartridge.h>
 
-nes::PpuBus::PpuBus(Cartridge & cartridge) {
-  this->cartridge = &cartridge;
-}
+namespace nes {
 
-void nes::PpuBus::write(WORD address, BYTE value) {
-  if (address > 0x3F00)
-    writeToPalette(address, value);
-  else if (address > 0x2000)
-    ram[(address - 0x2000) % 0x2FFF] = value;
-  else
-    cartridge->write(address, value);
-}
+  PpuBus::PpuBus(Cartridge & cartridge) {
+    this->cartridge = &cartridge;
+  }
 
-BYTE nes::PpuBus::read(WORD address) {
-  if (address > 0x3F00)
-    return paletteIndexes[(address - 0x3F00) % 0x3F1F];
-  if (address > 0x2000)
-    return ram[(address - 0x2000) % 0x2FFF];
-  else
-    return cartridge->read(address);
-}
+  void PpuBus::write(WORD address, BYTE value) {
+    if (address > 0x3F00)
+      writeToPalette(address, value);
+    else if (address > 0x2000)
+      ram[(address - 0x2000) % 0x2FFF] = value;
+    else
+      cartridge->write(address, value);
+  }
 
-WORD nes::PpuBus::read16(WORD address) {
-  WORD lsb = read(address);
-  WORD msb = read(address + 1);
-  return (msb << 8) | lsb;
-}
+  BYTE PpuBus::read(WORD address) {
+    if (address > 0x3F00)
+      return paletteIndexes[(address - 0x3F00) % 0x3F1F];
+    if (address > 0x2000)
+      return ram[(address - 0x2000) % 0x2FFF];
+    else
+      return cartridge->read(address);
+  }
 
-void nes::PpuBus::writeToPalette(WORD address, BYTE value) {
-  int index = (address - 0x3F00) % 0x3F1F;
-  if (index % 4 == 0) {
-    for (int i = 0; i < 32; i += 4)
-      paletteIndexes[i] = value;
-  } else {
-    paletteIndexes[index] = value;
+  WORD PpuBus::read16(WORD address) {
+    WORD lsb = read(address);
+    WORD msb = read(address + 1);
+    return (msb << 8) | lsb;
+  }
+
+  void PpuBus::writeToPalette(WORD address, BYTE value) {
+    int index = (address - 0x3F00) % 0x3F1F;
+    if (index % 4 == 0) {
+      for (int i = 0; i < 32; i += 4)
+        paletteIndexes[i] = value;
+    } else {
+      paletteIndexes[index] = value;
+    }
   }
 }
